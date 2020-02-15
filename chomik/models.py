01a1e0299823d324a3c1fd68eaa86bc079085542
Ynush <januszjanowski@outlook.com>
2020-02-15 16:23:24 +0100

    chomiki atakuja

--- chomik/models.py
+++ chomik/models.py
@@ -1,3 +1,4 @@
+from django.contrib.postgres.fields import JSONField
 from django.db import models
 
 class Sala(models.Model):
@@ -13,3 +14,24 @@ class Reserwacja(models.Model):
     sala = models.ForeignKey(Sala, on_delete=models.CASCADE, related_name='rezerwacje')
     date = models.DateField()
     comment = models.TextField()
+
+
+
+class SprzetKomputerowy(models.Model):
+    producent = models.CharField(max_length=64)
+    waga = models.PositiveIntegerField() # W gramach
+    sn = models.CharField(max_length=64, null=True)
+    class Meta:
+        abstract=True
+
+class Pecet(SprzetKomputerowy):
+    zasilacz = models.CharField(max_length=64)
+    karta_graficzna = models.CharField(max_length=64)
+
+class Laptop(SprzetKomputerowy):
+    ekran = models.CharField(max_length=64)
+    usb_count = models.CharField(max_length=64)
+
+
+class ChomikDżejson(models.Model):
+    json = JSONField()
